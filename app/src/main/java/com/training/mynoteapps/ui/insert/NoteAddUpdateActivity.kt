3036f112b72e872b632b8106825681748d4bb31a
Fauzan Pratama Putra <profauzan@gmail.com>
2022-05-20 15:07:09 +0700

    Initial commit

---
+++ app/src/main/java/com/training/mynoteapps/ui/insert/NoteAddUpdateActivity.kt
@@ -0,0 +1,133 @@
+package com.training.mynoteapps.ui.insert
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.lifecycle.ViewModelProvider
+import com.training.mynoteapps.R
+import com.training.mynoteapps.database.Note
+import com.training.mynoteapps.databinding.ActivityNoteAddUpdateBinding
+import com.training.mynoteapps.helper.DateHelper
+import com.training.mynoteapps.helper.ViewModelFactory
+
+class NoteAddUpdateActivity : AppCompatActivity() {
+    companion object {
+        const val EXTRA_NOTE = "extra_note"
+        const val ALERT_DIALOG_CLOSE = 10
+        const val ALERT_DIALOG_DELETE = 20
+    }
+    private var isEdit = false
+    private var note: Note? = null
+
+    private lateinit var noteAddUpdateViewModel: NoteAddUpdateViewModel
+
+    private var _activityNoteAddBinding : ActivityNoteAddUpdateBinding? = null
+    private val binding get() = _activityNoteAddBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        _activityNoteAddBinding = ActivityNoteAddUpdateBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+
+        noteAddUpdateViewModel = obtainViewModel(this@NoteAddUpdateActivity)
+
+        note = intent.getParcelableExtra(EXTRA_NOTE)
+        if (note != null) {
+            isEdit = true
+        } else {
+            note = Note()
+        }
+
+        binding?.btnSubmit?.setOnClickListener {
+            val title = binding?.edtTitle?.text.toString().trim()
+            val description = binding?.edtDescription?.text.toString().trim()
+            when {
+                title.isEmpty() -> {
+                    binding?.edtTitle?.error = getString(R.string.empty)
+                }
+                description.isEmpty() -> {
+                    binding?.edtDescription?.error = getString(R.string.empty)
+                }
+                else -> {
+                    note.let { note ->
+                        note?.title = title
+                        note?.description = description
+                    }
+                    if (isEdit) {
+                        noteAddUpdateViewModel.update(note as Note)
+                        showToast(getString(R.string.changed))
+                    } else {
+                        note.let { note ->
+                            note?.date = DateHelper.getCurrentDate()
+                        }
+                        noteAddUpdateViewModel.insert(note as Note)
+                        showToast(getString(R.string.added))
+                    }
+                    finish()
+                }
+            }
+        }
+    }
+
+    private fun showToast(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _activityNoteAddBinding = null
+    }
+    private fun obtainViewModel(activity: AppCompatActivity): NoteAddUpdateViewModel {
+        val factory = ViewModelFactory.getInstance(activity.application)
+        return ViewModelProvider(activity, factory).get(NoteAddUpdateViewModel::class.java)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        if (isEdit) {
+            menuInflater.inflate(R.menu.menu_form, menu)
+        }
+        return super.onCreateOptionsMenu(menu)
+    }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.action_delete -> showAlertDialog(ALERT_DIALOG_DELETE)
+            android.R.id.home -> showAlertDialog(ALERT_DIALOG_CLOSE)
+        }
+        return super.onOptionsItemSelected(item)
+    }
+    override fun onBackPressed() {
+        showAlertDialog(ALERT_DIALOG_CLOSE)
+    }
+
+    private fun showAlertDialog(type: Int) {
+        val isDialogClose = type == ALERT_DIALOG_CLOSE
+        val dialogTitle: String
+        val dialogMessage: String
+        if (isDialogClose) {
+            dialogTitle = getString(R.string.cancel)
+            dialogMessage = getString(R.string.message_cancel)
+        } else {
+            dialogMessage = getString(R.string.message_delete)
+            dialogTitle = getString(R.string.delete)
+        }
+        val alertDialogBuilder = AlertDialog.Builder(this)
+        with(alertDialogBuilder) {
+            setTitle(dialogTitle)
+            setMessage(dialogMessage)
+            setCancelable(false)
+            setPositiveButton(getString(R.string.yes)) { _, _ ->
+                if (!isDialogClose) {
+                    noteAddUpdateViewModel.delete(note as Note)
+                    showToast(getString(R.string.deleted))
+                }
+                finish()
+            }
+            setNegativeButton(getString(R.string.no)) { dialog, _ -> dialog.cancel() }
+        }
+        val alertDialog = alertDialogBuilder.create()
+        alertDialog.show()
+    }
+}
---
+++ app/src/main/java/com/training/mynoteapps/ui/main/MainActivity.kt
@@ -0,0 +1,57 @@
+package com.training.mynoteapps.ui.main
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.inputmethod.InputBinding
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.training.mynoteapps.R
+import com.training.mynoteapps.databinding.ActivityMainBinding
+import com.training.mynoteapps.helper.ViewModelFactory
+import com.training.mynoteapps.ui.insert.NoteAddUpdateActivity
+
+class MainActivity : AppCompatActivity() {
+
+    private var _activityMainBinding: ActivityMainBinding? = null
+    private val binding get() = _activityMainBinding
+
+    private lateinit var adapter: NoteAdapter
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        _activityMainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+
+        val mainViewModel = obtainViewModel(this@MainActivity)
+        mainViewModel.getAllNotes().observe(this) { noteList ->
+            if (noteList != null) {
+                adapter.setListNotes(noteList)
+            }
+        }
+
+        adapter = NoteAdapter()
+
+        binding?.rvNotes?.layoutManager = LinearLayoutManager(this)
+        binding?.rvNotes?.setHasFixedSize(true)
+        binding?.rvNotes?.adapter = adapter
+
+        binding?.fabAdd?.setOnClickListener { view ->
+            if (view.id == R.id.fab_add) {
+                val intent = Intent(this@MainActivity, NoteAddUpdateActivity::class.java)
+                startActivity(intent)
+            }
+        }
+    }
+
+    private fun obtainViewModel(activity: AppCompatActivity): MainViewModel {
+        val factory = ViewModelFactory.getInstance(activity.application)
+        return ViewModelProvider(activity, factory).get(MainViewModel::class.java)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _activityMainBinding = null
+    }
+}
